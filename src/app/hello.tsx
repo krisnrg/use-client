d82d49f59627c215ccc317f2e9b797af33e0ad6f
Kristian Gonzalez <kristian.gonzalez@simpleviewinc.com>
2024-06-18 13:47:10 -0700

    added notes

--- src/app/hello.tsx
+++ src/app/hello.tsx
@@ -3,16 +3,18 @@
 import react, { useEffect } from 'react';
 
 const Hello = () => {
-
     useEffect(() => {
         setTimeout(() => {
-          console.log('This only runs on client, if you remoev "use client" above you get an error cause this hook is here.')
+          console.log('This only runs on client, if you remove "use client" above you get an error cause this hook is here.')
         }, 1000);
       });
       
     console.log('This runs on both server and client.');
     return (
-        <div>Hello world from Next 14</div>
+        <div>
+            <h1>Hello world from Next 14</h1>
+            <p>view source some html, that seems to be the html that is "pre-rendered" on the server for Hello component</p>
+        </div>
     )
 }
 
