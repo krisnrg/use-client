71bb5c11ace6193b33c448768f9e9cda0c674a5a
Kristian Gonzalez <kristian.gonzalez@simpleviewinc.com>
2024-06-18 13:33:38 -0700

    fist commit, added hello component

---
+++ src/app/hello.tsx
@@ -0,0 +1,11 @@
+
+import react from 'react';
+
+const Hello = () => {
+    console.log('this is running on the server too');
+    return (
+        <div>Hello world from Next 14, using 'use client'</div>
+    )
+}
+
+export default Hello;
